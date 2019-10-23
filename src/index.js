ce62ad560427b452cc9c3837bf19d66e20d3f1f5
lariicsa@gremlim (from Travis CI) <deploy@travis-ci.org>
2019-10-23 00:49:40 +0000

    Deploy cv.lara.dev to github.com/Lariicsa/lariicsav3.github.io.git:master

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './containers/App';
-import styles from './scss/index.scss'
-import Router from './Router';
-
-ReactDOM.render(<Router />, document.getElementById('app'));
